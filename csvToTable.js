9c637ec1f30a32b4a99e6dfba427c29295cae15f
msantos1597 <msantos@luc.edu>
2022-12-06 01:10:39 -0600

    changes to support.html

--- csvToTable.js
+++ csvToTable.js
@@ -1,98 +1,105 @@
-function handleFiles(files) {
-	// Check for the various File API support.
-	if (window.FileReader) {
-		// FileReader are supported.
-		getAsText(files[0]);
-	} else {
-		alert('FileReader are not supported in this browser.');
+
+(function(){
+
+	// Constructor method
+	this.CsvToTable = function(){
+		this.csvFile = null;
+
+		// Create options by extending defaults with the passed in arugments
+    	if (arguments[0] && typeof arguments[0] === "object") {
+      		this.options = arguments[0];
+    	}
+
 	}
-}
 
-function getAsText(fileToRead) {
-	var reader = new FileReader();
-	// Handle errors load
-	reader.onload = loadHandler;
-	reader.onerror = errorHandler;
-	// Read file into memory as UTF-8      
-	reader.readAsText(fileToRead);
-}
-
-function loadHandler(event) {
-	var csv = event.target.result;
-	processData(csv);             
-}
-
-function processData(csv) {
-    var allTextLines = csv.split(/\r\n|\n/);
-    var lines = [];
-    while (allTextLines.length) {
-        lines.push(allTextLines.shift().split(','));
-    }
-	console.log(lines);
-	drawOutput(lines);
-}
-
-//if your csv file contains the column names as the first line
-function processDataAsObj(csv){
-    var allTextLines = csv.split(/\r\n|\n/);
-    var lines = [];
-	
-    //first line of csv
-    var keys = allTextLines.shift().split(',');
-	
-    while (allTextLines.length) {
-        var arr = allTextLines.shift().split(',');
-        var obj = {};
-        for(var i = 0; i < keys.length; i++){
-            obj[keys[i]] = arr[i];
+	CsvToTable.prototype.run = function() {
+		return buildTable.call(this);
 	}
-        lines.push(obj);
-    }
-        console.log(lines);
-	drawOutputAsObj(lines);
-}
 
-function errorHandler(evt) {
-	if(evt.target.error.name == "NotReadableError") {
-		alert("Canno't read file !");
-	}
-}
+	function getCSV() {
+		try{
+			var csvfile = this.options.csvFile;
+			return new Promise(function(resolve, reject) {
+				var request = new XMLHttpRequest();
+				request.open("GET", csvfile, true);
+				request.onload = function() {
+				    if (request.status == 200) {
+				        resolve(request.response);
+				    } else {
+				        reject(Error(request.statusText));
+				    }
+				};
 
-function drawOutput(lines){
-	//Clear previous data
-	document.getElementById("output").innerHTML = "";
-	var table = document.createElement("table");
-	for (var i = 0; i < lines.length; i++) {
-		var row = table.insertRow(-1);
-		for (var j = 0; j < lines[i].length; j++) {
-			var firstNameCell = row.insertCell(-1);
-			firstNameCell.appendChild(document.createTextNode(lines[i][j]));
+				request.onerror = function() {
+				 	reject(Error('Error fetching data.'));
+				};
+				request.send();
+			});
+		}catch(err){
+			console.error(err);
 		}
 	}
-	document.getElementById("output").appendChild(table);
-}
 
-//draw the table, if first line contains heading
-function drawOutputAsObj(lines){
-	//Clear previous data
-	document.getElementById("output").innerHTML = "";
-	var table = document.createElement("table");
-	
-	//for the table headings
-	var tableHeader = table.insertRow(-1);
- 	Object.keys(lines[0]).forEach(function(key){
- 		var el = document.createElement("TH");
-		el.innerHTML = key;		
-		tableHeader.appendChild(el);
-	});	
-	
-	//the data
-	for (var i = 0; i < lines.length; i++) {
-		var row = table.insertRow(-1);
-		Object.keys(lines[0]).forEach(function(key){
-			var data = row.insertCell(-1);
-			data.appendChild(document.createTextNode(lines[i][key]));
+    function isNotEmpty(row) {
+        return row !== "";
+    }
+
+    // polyfill `.filter()` for ECMAScript <5.1
+    // `f` must be pure (not modify original array).
+    if (!Array.prototype.filter) {
+      Array.prototype.filter = function(f) {
+        "use strict";
+        var p = arguments[1];
+        var o = Object(this);
+        var len = o.length;
+        for (var i = 0; i < len; i++) {
+          if (i in o) {
+              var v = o[i];
+              f.call(p, v, i, o);
+          }
+        }
+
+        return this;
+      };
+    }
+
+	function buildTable() {
+		getCSV.call(this).then(function(response){
+			var allRows = response.split(/\r?\n|\r/).filter(isNotEmpty);
+	        var table = '<table>';
+	        for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+	            if (singleRow === 0) {
+	                table += '<thead>';
+	                table += '<tr>';
+	            } else {
+	                table += '<tr>';
+	            }
+	            var rowCells = allRows[singleRow].split(',');
+	            for(var rowCell = 0; rowCell < rowCells.length; rowCell++){
+	                if(singleRow === 0){
+	                    table += '<th>';
+	                    table += rowCells[rowCell];
+	                    table += '</th>';
+	                } else {
+	                    table += '<td>';
+	                    table += rowCells[rowCell];
+	                    table += '</td>';
+	                }
+	            }
+	            if (singleRow === 0) {
+	                table += '</tr>';
+	                table += '</thead>';
+	                table += '<tbody>';
+	            } else {
+	                table += '</tr>';
+	            }
+	        }
+	        table += '</tbody>';
+	        table += '</table>';
+
+	        document.body.innerHTML += table;
+		}, function(error){
+			console.error(error);
 		});
 	}
-	document.getElementById("output").appendChild(table);
-}
+}());
